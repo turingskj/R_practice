c51ff5760e2d8e68f3ad67ab2fc1e1298728c7c7
turingskj <turingskj@hotmail.com>
2020-03-31 21:56:40 -0400

    updated many lines

--- runingR.r
+++ runingR.r
@@ -10,20 +10,150 @@ sink("test.out")
 # sink() will redirect to console output
 
 
-# c() function creates a list of vector with character string
-x <- c(1, 2.3, 2, 5, 2.2, 4)
+# R object times
+# numeric, integer, character, logical, complex
 
-y <- c(1:10)
+# Variable
+# A variable starts with ".", like .var, is a global variable that cannot be shown by ls().
+# one needs to put ls(all.names = TRUE)
+ls(all.names = TRUE)
+ls(name = .GlobalEnv, all.names= TRUE)
+ls(name = "package:base", all.names = TRUE)
 
-z <-c(1:4, "text", 4.2)
+# R data type
+# vector, matrix, list
+
+# c() function creates a vector. If all members of c() is numeric, the result is a numeric vector.
+# if all members are text string, result is an vector of strings. If members of c() is TURE or FALSE, the 
+# result is logical. If the members of c() are mixed (string, numbers, etc.), the result is the vector of
+# string.
+# If the members of c() is mixed, highest type is slected: character > numeric > integer > logical
+
+x <- c(1, 2.3, 2, 5, 2.2, 4L) #if mixed with numeric and interger, output is numeric
+
+y <- c(1:10) # default integer output
+
+z <-c(1:4, "text", 4.2)  # test stirng vector
+
+z <-c(TRUE, FALSE, TRUE)  # logical output
+
+z <-c(TRUE, 1.0, 3L)
+class(z) # the result is numeric
+
+
+# string
+a <- 'Start and end with single quote'
+print(a)
+
+b <- "Start and end with double quotes"
+print(b)
+
+c <- "single quote ' in between double quotes"
+print(c)
+
+d <- 'Double quotes " in between single quote'
+print(d)
+
+
+# using vector()
+x <- vector(mode="numeric", length=10L)
+y <- vector(mode = "integer", length = 3L)
+z <- vector(mode="logical", length=3L)
+
+
+# naming a vector
+fruit <- c(1, 2, 3, 5, 6)
+colnames(fruit)<-c("one", "two", "three", "four", "five")
+# colnames will produce error since the fruit is a vector; it should be a matrix or dataframe
+
+names(fruit)<-c("orange", "apple", "pear", "grape", "melon")
+fruit
+fruit["apple"]
+fruit[c("apple", "melon")]
+
+
+
+
+
+# use as.numeric(), as.integer(), as.character(), as.logical to change the class
+
+a <-c(1, 2, 0, "1")
+as.logical(a)
+# [1] NA NA NA NA
+a
+#[1] "1" "2" "0" "1"
+
+b <-as.logical(as.numeric(a))
+b
+# [1]  TRUE  TRUE FALSE  TRUE
+
+
+
+# lsit() will create a list of objects with different class (integer, numeric, etc)
+# use double bracket [[]] to acess the content of the list element
+# single bracket will give you the object class, not the object itself
+
+a<-list("a", 23.2, 3L, "home", TRUE)
+a[2]*2
+# Error in a[2] * 2 : non-numeric argument to binary operator
+
+a[[2]]*2
+# [1] 46.4
+
+print(a)
+
+
+
+# Matrix is the same as vector with different dimensions
+
+a <- matrix(1:10, nrow=2, ncol=5, byrow = FALSE)
+print(a)
+a[,3]
+a[1, 2:3]
+a[, 2:4]
+a[, c(2, 5)]
+a[-1, ]  # select all rows except 1
+
+a[1, ] # this will give a vector, not matrix
+a[1, , drop=FALSE] # this will give you a matrix of 1 by 5
+
+# naming matrix dimensions. the name must be a list
+mynames <- list(c("row1", "row2"), paste0("col", rep(c(1:5))))
+dimnames(a) <- mynames
+
+# access matrix by names
+a["row1", ] # first row of a
+a[, c("col1", "col3")]
+
+
+# another way to create a matrix
+a <- 1:20
+print(a)
+dim(a) <- c(4, 5) # change the vector of length 20 to a matrix of 4 by 5
+print(a)
+
+# creating a matrix from vectors
+a <-c(1:6)
+b <-c(31:36)
+c <-cbind(a, b)
+print(c)
+
+c <- rbind(a, b)
+c <- rbind(c, c["b", ])
+print(c)
+
+rownames(c)[3] <-"c"
+print(c)
+
+
+
+
+# using seq() and rep() for generating sequence
 
 y <- seq(1, 30, by=2)
 y <- seq(from=2, by=2, length=3)
 
-# using vector()
-x <- vector(mode="double", length=10L)
-y <- vector(mode = "integer", length = 3L)
-z <- vector(mode="logical", length=3L)
+
 
 # creating a sequence of variable names using rep() and seq()
 s <- rep(c(2, 3, 4), times=4)
@@ -54,7 +184,8 @@ is.nan(c(1, 2, 2, NA, NaN))
 
 a<-paste(c("X", "Y"), 1:10, sep="")
 b<-paste("X", 1:20, sep="")
-a = paste(c("x", "y"), rep(c(1, 2), each=2),sep="")
+a <- paste(c("x", "y"), rep(c(1, 2), each=2),sep="")
+a <- paste0(c("x", "y"), rep(c(1, 2), each=2))   # this and the above line is the same
 
 
 x <-c(1, 2, 3, 4, 5, 3, NA)
@@ -91,15 +222,7 @@ b<-paste("simple", 2, "r", 323) # paste combines all arguments as string.
 b<-x[-(1:2)]
 
 
-# naming a vector
-fruit <- c(1, 2, 3, 5, 6)
-colnames(fruit)<-c("one", "two", "three", "four", "five")
-# colnames will produce error since the fruit is a vector; it should be a matrix or dataframe
 
-names(fruit)<-c("orange", "apple", "pear", "grape", "melon")
-fruit
-fruit["apple"]
-fruit[c("apple", "melon")]
 
 
 # empty nemeric and character variables
@@ -121,18 +244,6 @@ z<-as.integer(digits)
 
 
 
-# string
-a <- 'Start and end with single quote'
-print(a)
-
-b <- "Start and end with double quotes"
-print(b)
-
-c <- "single quote ' in between double quotes"
-print(c)
-
-d <- 'Double quotes " in between single quote'
-print(d)
 
 
 
