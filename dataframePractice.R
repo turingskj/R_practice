fa785747874544244c685ba11499a4b3276a5abc
turingskj <turingskj@hotmail.com>
2020-04-26 03:11:50 -0400

    Dataframe subsetting

--- dataframePractice.R
+++ dataframePractice.R
@@ -16,9 +16,15 @@ sex <-c(rep(c("M", "F"), times=length(mynum)/2))
 mydata <- data.frame(mynum, sex)
 print(mydata)
 
+mydata2 <- mydata[c("sex")]  # select "sex" column
+
+print(mydata2)
+
 # changing variable (column) names
 names(mydata)  # will gives you a vector of characters
 names(mydata)[1] <- "year" # changes the first variable "mynum" to "year"
+mydata
+
 
 # or, to be safe, instead of the number, we can find the variable name and replace it
 names(mydata)[names(mydata)=="year"] <- "Year"
@@ -26,7 +32,7 @@ names(mydata)[names(mydat)=="sex"] <- "Sex"
 # add a new variable
 mydata$country <- c(rep(c("US", "KR"), times=3))
 
-
+# creating variable names and assign to the dataframe
 data2 <- data.frame(Group = paste0(rep(c("NC", "SZ", "BD"), each=10), sprintf("_%02d",seq(1:10))))
 data2
 
@@ -72,17 +78,13 @@ tDCS1_df$Stimulation <-factor(tDCS1_df$Stimulation, levels = 1:3, labels = c("Sh
 tDCS1_df$subject <-factor(tDCS1_df$subject, levels = 1:12)
 tDCS1_df$Difficulty <-factor(tDCS1_df$Difficulty, levels = 1:3, labels = c("Easy", "Moderate", "Difficult"))
 
+
 # change OriginalID variable to factor
 
-temp1<-table(tDCS1_df$OriginalID) # tabulate OriginalID by the possible value
-temp1<-attributes(temp1)$dimnames[[1]] # get the list of table headings (levels)
-tDCS1_df$OriginalID <-factor(tDCS1_df$OriginalID, levels = temp1) # set the factor levels
 
 temp1<-table(tDCS1_df$OriginalID) # tabulate by the value of OriginalID
 temp1<-attributes(temp1)$dimnames[[1]] # get the list of table headings (levels)
 tDCS1_df$OriginalID <-factor(tDCS1_df$OriginalID, levels = temp1)
-
-
 
 
 numberR <- nrow(tDCS1_df)
@@ -91,5 +93,43 @@ tail(tDCS1_df, n = 10L)
 str(tDCS1_df)
 
 
+# subsetting
 
+tDCS1_df2 <- tDCS1_df[-1] # remove the first variable
+head(tDCS_df2, n=15)
+head(tDCS1_df2, n=-410)
+
+tDCS1_df2 <- tDCS1_df[c(-1, -3)] # remove the first variable
+head(tDCS1_df2)
+
+myvar <- names(tDCS1_df)[c(2:5, 7)] # 
+tDCS1_df3 <- tDCS1_df[myvar]
+head(tDCS1_df3)
+
+myvar <- names(tDCS1_df)[2:7] # 
+tDCS1_df3 <- tDCS1_df[myvar]
+head(tDCS1_df3)
+
+tDCS1_df3 <-tDCS1_df[c("RT", "subject", "Difficulty")]
+head(tDCS1_df3)
+
+tDCS1_df3 <-tDCS1_df[c("RT", "subject", "Difficulty")]
+head(tDCS1_df3)
+
+# subsetting
+
+tDCS1_df3 <- subset(tDCS1_df, select=-OriginalID)
+head(tDCS1_df3)
+
+tDCS1_df3 <- subset(tDCS1_df, select=subject:RT)
+head(tDCS1_df3)
+
+tDCS1_df3 <-subset(tDCS1_df, select=c(subject, RT))
+head(tDCS1_df3)
+
+tDCS1_df3 <-subset(tDCS1_df, Stimulation==c("Anode", "Sham"), select=c(subject, Stimulation, Target, RT))
+head(tDCS1_df3, n=20)
+
+tDCS1_df3 <-subset(tDCS1_df, Stimulation==c("Anode", "Sham") & RT < 400, select=c(subject, Stimulation, Target, RT))
+head(tDCS1_df3, n=20)
 
